356d0b0f503cef4b3dd47a190b667cd8f0d76ef1
Anton Chernov <Anton940800@yandex.ru>
2017-09-02 19:17:26 +0500

    добавлен слайдер siema

---
+++ assets/js/main.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+	const body = $('body');
+
+
+	body.on('mouseover', '.logo', function () {
+		$('.header').addClass('header-hover');
+	});
+	body.on('mouseout', '.logo', function () {
+		$('.header').removeClass('header-hover');
+	});
+});
+const sliders = document.querySelectorAll('.country-item');
+Siema.prototype.addArrows = function() {
+	this.prevArrow = document.createElement('button');
+	this.nextArrow = document.createElement('button');
+	this.prevArrow.classList.add('prev-button');
+	this.nextArrow.classList.add('next-button');
+
+	this.selector.appendChild(this.prevArrow);
+	this.selector.appendChild(this.nextArrow);
+
+	this.prevArrow.addEventListener('click', () => this.prev());
+	this.nextArrow.addEventListener('click', () => this.next());
+}
+for(const siema of sliders) {
+	const instance = new Siema({
+		selector: siema,
+		loop: true
+	});
+	instance.addArrows();
+}
--- gulpfile.js
+++ gulpfile.js
@@ -41,7 +41,7 @@ gulp.task('html', function() {
 
 // Compile img into browsers
 gulp.task('img', function() {
-	return gulp.src("assets/img/*.*")
+	return gulp.src("assets/img/**/*.*")
 		.pipe(gulp.dest("public/img/"))
 		.pipe(browserSync.stream());
 });
@@ -56,30 +56,31 @@ gulp.task('js', function() {
 	return gulp.src(
 		[
 			'assets/js/jquery-3.2.1.min.js',
+			'assets/js/siema.min.js',
 			'assets/js/main.js'
 		])
 		.pipe(sourcemaps.init())
 		.pipe(concat('main.min.js'))
-		.pipe(uglify({
-			sequences     : true,  // join consecutive statemets with the “comma operator”
-			properties    : true,  // optimize property access: a["foo"] ? a.foo
-			dead_code     : true,  // discard unreachable code
-			drop_debugger : true,  // discard “debugger” statements
-			unsafe        : false, // some unsafe optimizations (see below)
-			conditionals  : true,  // optimize if-s and conditional expressions
-			comparisons   : true,  // optimize comparisons
-			evaluate      : true,  // evaluate constant expressions
-			booleans      : true,  // optimize boolean expressions
-			loops         : true,  // optimize loops
-			unused        : true,  // drop unused variables/functions
-			hoist_funs    : true,  // hoist function declarations
-			hoist_vars    : true, // hoist variable declarations
-			if_return     : true,  // optimize if-s followed by return/continue
-			join_vars     : true,  // join var declarations
-			cascade       : true,  // try to cascade `right` into `left` in sequences
-			side_effects  : true,  // drop side-effect-free statements
-			warnings      : true  // warn about potentially dangerous optimizations/code
-		}))
+		// .pipe(uglify({
+		// 	sequences     : true,  // join consecutive statemets with the “comma operator”
+		// 	properties    : true,  // optimize property access: a["foo"] ? a.foo
+		// 	dead_code     : true,  // discard unreachable code
+		// 	drop_debugger : true,  // discard “debugger” statements
+		// 	unsafe        : false, // some unsafe optimizations (see below)
+		// 	conditionals  : true,  // optimize if-s and conditional expressions
+		// 	comparisons   : true,  // optimize comparisons
+		// 	evaluate      : true,  // evaluate constant expressions
+		// 	booleans      : true,  // optimize boolean expressions
+		// 	loops         : true,  // optimize loops
+		// 	unused        : true,  // drop unused variables/functions
+		// 	hoist_funs    : true,  // hoist function declarations
+		// 	hoist_vars    : true, // hoist variable declarations
+		// 	if_return     : true,  // optimize if-s followed by return/continue
+		// 	join_vars     : true,  // join var declarations
+		// 	cascade       : true,  // try to cascade `right` into `left` in sequences
+		// 	side_effects  : true,  // drop side-effect-free statements
+		// 	warnings      : true  // warn about potentially dangerous optimizations/code
+		// }))
 		.pipe(sourcemaps.write())
 		.pipe(gulp.dest("public/js/"))
 		.pipe(browserSync.stream());
