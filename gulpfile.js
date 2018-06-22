50bb87ca8fe723929a6b0997e9751716f2ed4c58
Anton Chernov <anton940800@yandex.ru>
2018-06-23 01:11:36 +0500

    added package.json; edit gitignore; edit gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,11 @@ const sass = require('gulp-sass');
 const prefixer = require('gulp-autoprefixer');
 const cssmin = require('gulp-minify-css');
 const sourcemaps = require('gulp-sourcemaps');
-const uglify = require('gulp-uglify');
 const concat = require('gulp-concat');
 const browserSync = require('browser-sync').create();
-const babel = require('gulp-babel');
 
 // Static Server + watching scss/html files
-gulp.task('server', ['sass', 'babel', 'js', 'html', 'img', 'fonts'], function() {
+gulp.task('server', ['sass', 'js', 'html', 'img', 'fonts'], function() {
 
 	browserSync.init({
 		server: "./public"
@@ -51,14 +49,6 @@ gulp.task('fonts', function() {
 		.pipe(browserSync.stream());
 });
 
-gulp.task('babel', () =>
-	gulp.src('assets/js/es6.js')
-		.pipe(babel({
-			presets: ['env']
-		}))
-		.pipe(gulp.dest('assets/js/babel'))
-);
-
 // Compile js auto-inject into browsers
 gulp.task('js', function() {
 	return gulp.src(
@@ -70,26 +60,6 @@ gulp.task('js', function() {
 		])
 		.pipe(sourcemaps.init())
 		.pipe(concat('main.min.js'))
-		// .pipe(uglify({
-		// 	sequences     : true,  // join consecutive statemets with the “comma operator”
-		// 	properties    : true,  // optimize property access: a["foo"] ? a.foo
-		// 	dead_code     : true,  // discard unreachable code
-		// 	drop_debugger : true,  // discard “debugger” statements
-		// 	unsafe        : false, // some unsafe optimizations (see below)
-		// 	conditionals  : true,  // optimize if-s and conditional expressions
-		// 	comparisons   : true,  // optimize comparisons
-		// 	evaluate      : true,  // evaluate constant expressions
-		// 	booleans      : true,  // optimize boolean expressions
-		// 	loops         : true,  // optimize loops
-		// 	unused        : true,  // drop unused variables/functions
-		// 	hoist_funs    : true,  // hoist function declarations
-		// 	hoist_vars    : true, // hoist variable declarations
-		// 	if_return     : true,  // optimize if-s followed by return/continue
-		// 	join_vars     : true,  // join var declarations
-		// 	cascade       : true,  // try to cascade `right` into `left` in sequences
-		// 	side_effects  : true,  // drop side-effect-free statements
-		// 	warnings      : true  // warn about potentially dangerous optimizations/code
-		// }))
 		.pipe(sourcemaps.write())
 		.pipe(gulp.dest("public/js/"))
 		.pipe(browserSync.stream());
